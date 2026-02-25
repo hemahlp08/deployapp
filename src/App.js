af4d81e2b730f3fea8dc83cc1a77a80a364696ae
Hema <hema.hlp08@gmail.com>
2026-02-25 11:16:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+import { BrowserRouter } from "react-router-dom"
+import { Route } from "react-router-dom"
+import { Routes } from "react-router-dom"
+import Home from "./Home"
+import Reg from "./Register"
+
+function App()
+{
+  return(
+    <>
+
+      <h1>I am App</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/reg" element = {<Reg/>}></Route>
+        <Route path = "/h" element = {<Home/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/home.jsx
@@ -0,0 +1,9 @@
+function Home()
+{
+    return(
+        <>
+        <h1>I am App</h1>
+        </>
+    )
+}
+export default Home
